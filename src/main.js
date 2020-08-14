f1ec0f1118ba3ca3a4c94666e86004cdb24ce512
z242 <1160505603@qq.com>
2020-08-14 15:22:00 +0800

    first

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
+import Echarts from 'echarts'
 
+Vue.use(ElementUI)
+Vue.prototype.$echarts=Echarts;
+Vue.prototype.$bus=new Vue();
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
